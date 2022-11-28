6c0cc108c7b07802bdc3dff25597130081b56810
mecaneer23 <mecaneer23@gmail.com>
2022-11-28 11:16:08 -0600

    fix color themes

--- resources/tools.js
+++ resources/tools.js
@@ -12,17 +12,32 @@ function inner_navbar(item) {
     }
 }
 
-function setPair(key, value) {
-    this.setProperty(key, value);
-    return this;
+function set_colorscheme(event, theme) {
+    if (theme !== null) {
+        document.body.setAttribute("data-theme", theme);
+        return;
+    }
+    if (event.matches) {
+        document.body.setAttribute("data-theme", "dark");
+    } else {
+        document.body.setAttribute("data-theme", "light");
+    }
 }
 
-function to_dark_mode() {
-    let root = document.querySelector(":root").style
-    root.setPair("--")
-    // getpropertyvalue
+function toggle_colorscheme(event) {
+    let theme = document.body.getAttribute("data-theme");
+    if (theme == "dark") {
+        set_colorscheme(event, "light");
+    } else if (theme == "light") {
+        set_colorscheme(event, "dark");
+    } else {
+        console.log(theme);
+    }
 }
 
-function onload() {
+window.addEventListener("load", () => {
     refresh_navbar();
-}
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => set_colorscheme(e, null));
+    document.getElementById("toggle").addEventListener("click", toggle_colorscheme);
+    set_colorscheme(null, window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light");
+})
