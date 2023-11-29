8952cd15dcfca5c73887888dcbbc15ba6d184058
mecaneer23 <mecaneer23@gmail.com>
2023-11-29 10:39:40 -0600

    make include more readable

--- resources/include.js
+++ resources/include.js
@@ -1,16 +1,15 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("w3-include-html");
+    let elem, file, xhttp, allElements = document.getElementsByTagName("*");
+    for (let i = 0; i < allElements.length; i++) {
+        elem = allElements[i];
+        file = elem.getAttribute("w3-include-html");
         if (file) {
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    elmnt.removeAttribute("w3-include-html");
+                    if (this.status == 200) { elem.innerHTML = this.responseText; }
+                    if (this.status == 404) { elem.innerHTML = "Page not found."; }
+                    elem.removeAttribute("w3-include-html");
                     includeHTML();
                 }
             }
@@ -19,5 +18,5 @@ function includeHTML() {
             return;
         }
     }
-};
+}
 includeHTML();
