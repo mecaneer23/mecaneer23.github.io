b4c8df852450e101f950b556aaef9acb960d6c07
mecaneer23 <mecaneer23@gmail.com>
2024-03-07 12:55:07 -0600

    fix: zooming on all pages

--- resources/tools.js
+++ resources/tools.js
@@ -258,8 +258,11 @@ function load() {
         if (pageIs("portfolio")) {
             Array.from(document.getElementsByTagName("img")).forEach((img) => img.addEventListener("contextmenu", modal));
             window.addEventListener("click", closeModal);
-        } else if (pageIs("about") || pageIs("gallery")) {
+        }
+        if (pageIs("about")) {
             document.querySelector("img.profile").addEventListener("contextmenu", doABarrelRoll);
+        }
+        if (pageIs("about") || pageIs("gallery")) {
             Array.from(document.querySelectorAll("img")).forEach((img) => img.addEventListener("click", modal));
         }
         document.getElementById("modal").addEventListener("click", closeModal);
