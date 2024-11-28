e136f9999f83acc7e8239015f1360bf2090b82ee
mecaneer23 <mecaneer23@gmail.com>
2024-11-28 10:51:38 -0600

    feat!: implement typo correction for github repo redirection in url

---
+++ 404_test.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '/')));
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+// RUNNING:
+// npm install express --no-bin-links; node 404_test.js
