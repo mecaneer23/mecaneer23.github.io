8b475f8244249d43c28cf19b84707a9d2488ccf8
mecaneer23 <mecaneer23@gmail.com>
2025-01-29 11:48:08 +0200

    fix: don't iterate using forEach over non-array group

--- resources/tools.js
+++ resources/tools.js
@@ -389,7 +389,7 @@ function load() {
         setColorscheme(null, (localStorage.getItem("colorscheme") ? localStorage.getItem("colorscheme") == "dark" : window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light");
         document.getElementsByClassName("switch-container")[0].addEventListener("mouseover", weirdThemeHandler);
         document.getElementsByClassName("switch-container")[0].addEventListener("mouseout", weirdThemeHandlerSetFalse);
-        document.getElementsByClassName("nav-item").forEach((element) => element.addEventListener("focus", openMenu));
+        Array.from(document.getElementsByClassName("nav-item")).forEach((element) => element.addEventListener("focus", openMenu));
         document.getElementById("current-year").innerHTML = (new Date()).getFullYear();
         if (pageNameIncludes("portfolio")) {
             Array.from(document.getElementsByTagName("img")).forEach((img) => img.addEventListener("contextmenu", modal));
