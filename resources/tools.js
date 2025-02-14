49feb1038ff2425a631b831e740c65a605d3f4df
mecaneer23 <mecaneer23@gmail.com>
2025-02-14 16:08:22 +0200

    fix: loading screen should actually work correcly cross-browser

--- resources/tools.js
+++ resources/tools.js
@@ -456,20 +456,37 @@ function loader() {
     }
 }
 
+// Thanks to Yong Wang (https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists)
+function waitForElem(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+async function removeLoadingScreen() {
+    let loadingScreen;
+    loadingScreen = await waitForElem("#loading-screen");
+    loadingScreen.style.display = "none";
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+    }, 3000);
+}
+
 window.addEventListener("load", () => {
     loader();
-    const loadingScreen = document.getElementById("loading-screen");
-    loadingScreen.style.display = "none";
-    setTimeout(() => {
-        loadingScreen.style.display = "none";
-    }, 3000);
-});
-
-document.body.addEventListener("load", () => {
-    console.log("here")
-    const loadingScreen = document.getElementById("loading-screen");
-    loadingScreen.style.display = "none";
-    setTimeout(() => {
-        loadingScreen.style.display = "none";
-    }, 3000);
+    removeLoadingScreen();
 });
