79c99ba08237a5f8b3fe16b0b82e4f5aa21bf8ad
mecaneer23 <mecaneer23@gmail.com>
2025-03-07 15:34:55 +0200

    refactor: stricter ruff settings

--- server_with_404.py
+++ server_with_404.py
@@ -5,7 +5,7 @@ Run a local web server which supports 404 responses
 
 import http.server
 import socketserver
-import os
+from pathlib import Path
 
 PORT = 5500
 
@@ -15,20 +15,20 @@ class Handler404(http.server.SimpleHTTPRequestHandler):
     SimpleHTTPRequestHandler that handles a 404 response
     using a `404.html` file in the root site directory
     """
-    def do_GET(self):
-        if os.path.exists(self.translate_path(self.path)):
+    def do_GET(self) -> None:  # noqa: N802
+        if Path(self.translate_path(self.path)).exists():
             super().do_GET()
             return
         self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         try:
-            with open("404.html", "rb") as file:
+            with Path("404.html").open("rb") as file:
                 self.wfile.write(file.read())
         except FileNotFoundError:
             self.wfile.write(b"404 - 404 File Not Found")
 
 
 with socketserver.TCPServer(("", PORT), Handler404) as httpd:
-    print(f"Server running at http://localhost:{PORT}")
+    print(f"Server running at http://localhost:{PORT}")  # noqa: T201
     httpd.serve_forever()
