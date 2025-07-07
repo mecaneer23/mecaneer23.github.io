9bc45f5adca67f5e1312daf6ca13dd6c5c8ba53d
mecaneer23 <mecaneer23@gmail.com>
2025-07-06 23:37:28 -0500

    feat: begin parsing video title to better present information

--- auto-racing-video-gallery/build-video-gallery.js
+++ auto-racing-video-gallery/build-video-gallery.js
@@ -1,28 +1,61 @@
+function parseVideoTitle(title) {
+    let [name, car, event, track, driver] = title.split(' | ');
+    return {
+        name,
+        car,
+        event,
+        track,
+        driver,
+    }
+}
+
 async function buildGallery(videos) {
     const gallery = document.getElementById('video-gallery');
     for (let id of videos) {
-        const name = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${id}`)
+        const videoTitle = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${id}`)
             .then(response => response.json())
             .then(data => data.title);
 
-        const a = document.createElement('a');
-        a.classList.add('portfolio-card');
+        const card = document.createElement('a');
+        card.classList.add('portfolio-card');
+        card.href = `https://www.youtube.com/watch?v=${id}`;
+
+        let parsedTitle = parseVideoTitle(videoTitle);
 
         const thumbnail = document.createElement('img');
         thumbnail.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
         thumbnail.classList.add('thumbnail');
-        thumbnail.alt = `Thumbnail for ${name} video`;
+        thumbnail.alt = `Thumbnail for ${parsedTitle.name} video`;
+        card.appendChild(thumbnail);
 
         const title = document.createElement('p');
-        title.classList.add('video-title');
-        title.textContent = name;
+        title.classList.add('video-card-title');
+        title.textContent = parsedTitle.name;
+        card.appendChild(title);
 
-        a.href = `https://www.youtube.com/watch?v=${id}`;
+        const event = document.createElement('p');
+        event.classList.add('video-card-event');
+        event.textContent = parsedTitle.event;
+        card.appendChild(event);
 
-        a.appendChild(thumbnail);
-        a.appendChild(title);
+        const track = document.createElement('p');
+        track.classList.add('video-card-track');
+        track.textContent = parsedTitle.track;
+        card.appendChild(track);
 
-        gallery.appendChild(a);
+        const car = document.createElement('p');
+        car.classList.add('video-card-car');
+        car.textContent = parsedTitle.car;
+        card.appendChild(car);
+
+        if (parsedTitle.driver !== "Mecaneer23") {
+            const driver = document.createElement('p');
+            driver.classList.add('video-card-driver');
+            driver.textContent = parsedTitle.driver;
+            card.appendChild(driver);
+        }
+
+        gallery.appendChild(card);
     }
 }
 
